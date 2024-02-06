234b0061660a323d8763873afa1e49903364d61e
Rushikesh <rushikesh.belkunde@algorisys.com>
2024-02-06 17:01:06 +0530

    vercel db added

--- prisma/migrations/20240122060909_todo_update/migration.sql
+++
@@ -1,6 +0,0 @@
--- CreateEnum
-CREATE TYPE "status" AS ENUM ('ON_HOLD', 'COMPLETED', 'IN_PROGRESS');
-
--- AlterTable
-ALTER TABLE "Todo" ADD COLUMN     "bookmarked" BOOLEAN NOT NULL DEFAULT false,
-ADD COLUMN     "status" "status" NOT NULL DEFAULT 'IN_PROGRESS';
--- prisma/migrations/20240122063351_status_added/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "SubTodo" ADD COLUMN     "status" "status" NOT NULL DEFAULT 'IN_PROGRESS';
